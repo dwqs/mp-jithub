ed98bc38b799945e1cc453df05340f99a447803e
dwqs <461147874@qq.com>
2018-05-02 00:07:28 +0800

    feat: Add loading components

---
+++ src/components/loading/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="loading-wrapper">
+        <div class="lds-hourglass"></div>
+    </div>
+</template>
+
+<script>
+    import './index.less';
+    export default {
+        props: {
+            type: String,
+            default: 'global',
+            validator: val => ['global', 'partial'].includes(val)
+        }
+    };
+</script>
