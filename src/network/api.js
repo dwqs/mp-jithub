3558da586cb133b04b58beba426cc3b73715a3fb
dwqs <461147874@qq.com>
2018-05-12 18:56:42 +0800

    feat: 添加过滤接口

--- src/network/api.js
+++ src/network/api.js
@@ -4,5 +4,10 @@ export default {
     async getTrendingRepos (data = {}) {
         const res = await network.get(`/jithub/trending`, data);
         return res;
+    },
+
+    async filterTrendingRepos (lang, data = {}) {
+        const res = await network.get(`/jithub/trending/${lang}`, data);
+        return res;
     }
 };
