262f3fa56ad7b61c60e4acf4a05912c84c37190b
dwqs <461147874@qq.com>
2018-05-19 21:07:22 +0800

    feat: 添加 tabbar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './app';
 import store from './vuex/index';
+import './common.less';
 
 Vue.prototype.$store = store;
 Vue.config.productionTip = false;
@@ -13,13 +14,31 @@ export default {
     config: {
         pages: [
             '^pages/index/index',
-            'pages/lang/lang'
+            'pages/lang/lang',
+            'pages/rank/rank'
         ],
         window: {
             backgroundTextStyle: 'light',
-            navigationBarBackgroundColor: '#fff',
+            navigationBarBackgroundColor: '#FFFFFF',
             navigationBarTitleText: 'Jithub',
             navigationBarTextStyle: 'black'
+        },
+        tabBar: {
+            color: '#7A7E83',
+            selectedColor: '#2AB62F',
+            borderStyle: 'black',
+            backgroundColor: '#FFFFFF',
+            list: [{
+                pagePath: 'pages/index/index',
+                iconPath: '/static/images/trending.png',
+                selectedIconPath: '/static/images/trending-selected.png',
+                text: 'Trending'
+            }, {
+                pagePath: 'pages/rank/rank',
+                iconPath: '/static/images/rank.png',
+                selectedIconPath: '/static/images/rank-selected.png',
+                text: 'Ranking'
+            }]
         }
     }
 };
