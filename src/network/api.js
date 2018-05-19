30db69695ef69a02da07b6c0f823e382bd9a5272
dwqs <461147874@qq.com>
2018-05-19 21:07:41 +0800

    feat: 增加用户搜索相关 api

--- src/network/api.js
+++ src/network/api.js
@@ -1,4 +1,11 @@
 import network from './index';
+// token: https://developer.github.com/v3/#rate-limiting
+import { token } from '../../config/host';
+
+const auth = {
+    /* eslint-disable */
+    access_token: token
+};
 
 export default {
     async getTrendingRepos (data = {}) {
@@ -9,6 +16,20 @@ export default {
     async filterTrendingRepos (lang, data = {}) {
         lang = lang.split(' ').map(t => t.toLowerCase()).join('-');
         const res = await network.get(`/jithub/trending/${lang}`, data);
+        return res;
+    },
+
+    async getRankingItems (data) {
+        const params = {
+            ...data,
+            ...auth
+        };
+        const res = await network.get(`/search/users`, params);
+        return res;
+    },
+
+    async getUserInfo (username) {
+        const res = await network.get(`/users/${username}`, auth);
         return res;
     }
 };
