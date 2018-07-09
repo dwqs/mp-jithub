853b232a8fd52ac0d1f47622aee44b4b3a50ee9c
dwqs <461147874@qq.com>
2018-07-10 01:33:26 +0800

    feat: Add lang and date setting

--- src/pages/setting/index.vue
+++ src/pages/setting/index.vue
@@ -2,17 +2,109 @@
     <div class="setting-page">
         <top-header title="Setting" :back="true"></top-header>
         <div class="setting-wrap">
-            setting
+            <div class="setting-info">
+                <p class="setting-tip">选择默认语言</p>
+                <div class="setting-flex" @click="setDefLang">
+                    {{lang ? lang : 'All Languages'}}
+                    <span class="more-info">
+                        <i class="right-arrow"></i>
+                    </span>
+                </div>
+            </div>
+            <div class="setting-info">
+                <p class="setting-tip">选择默认周期</p>
+                <picker :range="dates" @change="dateChange" :value="val">
+                    <div class="setting-flex">
+                        {{since ? since : 'daily'}}
+                        <span class="more-info">
+                            <i class="right-arrow"></i>
+                        </span>
+                    </div>
+                </picker>
+            </div>
+            <p class="setting-empty"></p>
+            <div class="setting-info">
+                <p class="setting-tip">关于</p>
+                <div class="setting-flex">
+                    源代码
+                    <span class="more-info">
+                        https://github.com/dwqs/mp-jithub
+                    </span>
+                </div>
+                <div class="setting-flex no-margin">
+                    问题反馈
+                    <span class="more-info">
+                        https://github.com/dwqs/mp-jithub/issues
+                    </span>
+                </div>
+                <div class="setting-flex no-margin">
+                    当前版本
+                    <span class="more-info">
+                        v1.1.6
+                    </span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import { mapActions, mapGetters } from 'vuex';
+
     import topHeader from '@src/components/top-header/index.vue';
 
     export default {
+        data () {
+            return {
+                dates: ['daily', 'weekly', 'monthly'],
+                lang: '',
+                val: 0,
+                since: ''
+            };
+        },
+
+        methods: {
+            ...mapActions([
+                'saveSetting',
+                'filterTrendingReposBySetting',
+                'setTrendingLoading'
+            ]),
+
+            dateChange (e) {
+                this.val = e.mp.detail.value;
+                this.since = this.dates[this.val];
+                wx.setStorageSync('since', this.since);
+            },
+
+            setDefLang () {
+                wx.navigateTo({
+                    url: '../lang/lang?from=setting'
+                });
+            }
+        },
+
+        computed: {
+            ...mapGetters({
+                trending: 'getTrendingState'
+            })
+        },
+
         components: {
             topHeader
+        },
+
+        onShow () {
+            this.lang = this.trending.lang;
+            this.since = this.trending.since;
+            const index = this.dates.indexOf(this.trending.since);
+            this.val = index > -1 ? index : 0;
+        },
+
+        onUnload () {
+            this.saveSetting({
+                lang: this.lang ? this.lang : '',
+                since: this.since ? this.since : 'daily'
+            });
         }
     };
 </script>
@@ -25,5 +117,37 @@
         right: 0;
         bottom: 0;
         background-color: #f6f8f8;
+        padding-top: 25rpx;
+    }
+
+    .setting-info {
+        font-size: 24rpx;
+        margin-top: 25rpx;
+        .setting-tip {
+            color: rgb(121, 117, 117);
+            margin-left: 15rpx;
+        }
+    }
+
+    .setting-flex {
+        position: relative;
+        display: flex;
+        align-items: center;
+        height: 75rpx;
+        background: #fff;
+        margin-top: 15rpx;
+        padding-left: 15rpx;
+        &.no-margin {
+            margin: 0;
+            border-top: 1rpx solid #e1e4e8;
+        }
+        .setting-picker { 
+            
+        }
+    }
+
+    .setting-empty {
+        margin: 25rpx 0;
+        overflow: hidden;
     }
 </style>
